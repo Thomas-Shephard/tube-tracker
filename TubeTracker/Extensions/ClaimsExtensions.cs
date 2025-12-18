d4752029da67c563903cc114f74868d944ef60c7
Thomas Shephard <thomas@thomas-shephard.com>
2025-12-18 22:35:03 +0000

    Revert claims changes

--- TubeTracker/Extensions/ClaimsExtensions.cs
+++ TubeTracker/Extensions/ClaimsExtensions.cs
@@ -9,7 +9,7 @@ public static class ClaimsExtensions
     {
         public int? GetUserId()
         {
-            string? userIdString = user.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            string? userIdString = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             return int.TryParse(userIdString, out int userId)
                 ? userId
                 : null;
@@ -17,7 +17,7 @@ public static class ClaimsExtensions
 
         public string? GetUserEmail()
         {
-            return user.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+            return user.FindFirst(ClaimTypes.Email)?.Value;
         }
 
         public bool IsVerified()
@@ -28,7 +28,7 @@ public static class ClaimsExtensions
 
         public DateTime? GetExpirationTime()
         {
-            string? expString = user.FindFirst(JwtRegisteredClaimNames.Exp)?.Value;
+            string? expString = user.FindFirst(ClaimTypes.Expiration)?.Value;
             if (!long.TryParse(expString, out long expUnixTime))
                 return null;
             DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(expUnixTime);
